fd47ee0ee9894ea0fde00824feb40c34ac409046
Bhoomika Surve <survebhoomika@gmail.com>
2026-02-10 17:45:32 +0530

    Time: 2015 ms (35.43%) | Memory: 522.1 MB (50.32%) - LeetSync

---
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i < n; ++i){
+            unordered_set<int> vis;
+            int cnt[2] = {0, 0};
+            for (int j = i; j < n; ++j){
+                if (!vis.count(nums[j])){
+                    vis.insert(nums[j]);
+                    ++cnt[nums[j] & 1];
+                
+                
+                }
+                if (cnt[0] == cnt[1]){
+                    ans = max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
